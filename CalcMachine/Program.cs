beecfa82efc0d04bc92b32c66cfe7cd76db3cace
NuloSois <104397117+NuloSois@users.noreply.github.com>
2022-05-11 16:44:57 -0300

    Update Program.cs
    Support for Trigonometry

--- CalcMachine/Program.cs
+++ CalcMachine/Program.cs
@@ -1,46 +1,108 @@
-Console.WriteLine("Select The Two Numbers you wish to calculate");
-
-int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Select the number of the wanted operation");
+Console.WriteLine("Select the type of calculator you want to use");
 Console.WriteLine(@"
-  1-Addition  
-  2-Subtraction  
-  3-Multiplication  
-  4-Division
-  5-Exponentiation");
+  1-Arithmetic 
+  2-Trigonometric");
 
-string operation = Console.ReadLine() ?? "1";
+string CalculatorCore = Console.ReadLine() ?? "1";
 
-switch(operation) 
+switch(CalculatorCore)
+
 {
-  case "1":
-    int res = num1 + num2;
-    Console.WriteLine($"The Result of {num1} + {num2} is {res}");
-    break;
+case "1":
+  Console.WriteLine("Select The Two Numbers you wish to calculate");
 
-  case "2":
-    Console.WriteLine($"The Result of {num1} - {num2} is {num1 - num2}");
-    break;
+  int num1 = Convert.ToInt32(Console.ReadLine());
+  int num2 = Convert.ToInt32(Console.ReadLine());
 
-  case "3":
-    Console.WriteLine($"The Result of {num1} * {num2} is {num1 * num2}");
-    break;
+  Console.WriteLine("Select the number of the wanted operation");
+  Console.WriteLine(@"
+    1-Addition       (x+y) 
+    2-Subtraction    (x-y)
+    3-Multiplication (x*y) 
+    4-Division       (x/y)
+    5-Exponentiation (x^y)");
 
-  case "4":
-    float float1 = float.Parse(num1.ToString());
-    float float2 = float.Parse(num2.ToString());
-    Console.WriteLine($"The Result of {num1} / {num2} is {float1 / float2}");
-    break;
+  string operation = Console.ReadLine() ?? "1";
 
-  case "5":
-    Console.WriteLine($"The Result of {num1} ^ {num2} is {Math.Pow(num1, num2)}");
-    break;
+  switch(operation) 
+  {
+   case "1":
+      int res = num1 + num2;
+      Console.WriteLine($"The Result of {num1} + {num2} is {res}");
+     break;
+
+   case "2":
+      Console.WriteLine($"The Result of {num1} - {num2} is {num1 - num2}");
+      break;
+
+   case "3":
+      Console.WriteLine($"The Result of {num1} * {num2} is {num1 * num2}");
+      break;
+
+   case "4":
+      float float1 = float.Parse(num1.ToString());
+      float float2 = float.Parse(num2.ToString());
+      Console.WriteLine($"The Result of {num1} / {num2} is {float1 / float2}");
+      break;
+
+   case "5":
+      Console.WriteLine($"The Result of {num1} ^ {num2} is {Math.Pow(num1, num2)}");
+      break;
 
   default:
     Console.WriteLine("Invalid option");
     break;
+  }
+break;
+case "2":
+  Console.WriteLine("Write the value of the angle");
+
+  int num3 = Convert.ToInt32(Console.ReadLine());
+
+  Console.WriteLine("Select the number of the wanted trigonometric function");
+  Console.WriteLine(@"
+    1-Sine       
+    2-Cosine   
+    3-Tangent
+    4-Cosecant
+    5-Secant
+    6-Cotangent");
+
+  string TrigonometricFunction = Console.ReadLine() ?? "1";
+
+  double double3 = double.Parse(num3.ToString());
+  double radian3 = (double3 * (Math.PI)) / 180;
+  
+
+  switch(TrigonometricFunction) 
+  {
+
+    case "1":
+    Console.WriteLine($"The Sine of {double3} is {Math.Sin(radian3)}");
+    break;
+
+    case "2":
+    Console.WriteLine($"The Cosine of {double3} is {Math.Cos(radian3)}");
+    break;
+
+    case "3":
+    Console.WriteLine($"The Tangent of {double3} is {Math.Tan(radian3)}");
+    break;
+
+    case "4":
+    Console.WriteLine($"The Cosecant of {double3} is {1/(Math.Sin(radian3))}");
+    break;
+
+    case "5":
+    Console.WriteLine($"The Secant of {double3} is {1/(Math.Cos(radian3))}");
+    break;
+
+    case "6":
+    Console.WriteLine($"The Coangent of {double3} is {1/(Math.Tan(radian3))}");
+    break;
+  }
+
+break;
 }
 Console.ReadLine();
 /*Very special thanks to my friend Victória (EternalQuasar0206) for helping me with the refactoring
