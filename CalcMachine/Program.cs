f24cba78e808fed0f03d99ffa934be3c8d119e58
NuloSois <104397117+NuloSois@users.noreply.github.com>
2022-05-12 10:08:36 -0300

    Update Program.cs
    Addition of 2.1.1

--- CalcMachine/Program.cs
+++ CalcMachine/Program.cs
@@ -1,7 +1,8 @@
 Console.WriteLine("Select the type of calculator you want to use");
 Console.WriteLine(@"
   1-Arithmetic 
-  2-Trigonometric");
+  2-Trigonometric
+  3-Special");
 
 string CalculatorCore = Console.ReadLine() ?? "1";
 
@@ -9,10 +10,10 @@ switch(CalculatorCore)
 
 {
 case "1":
-  Console.WriteLine("Select The Two Numbers you wish to calculate");
+  Console.WriteLine("Write The Two Numbers you wish to calculate");
 
-  int num1 = Convert.ToInt32(Console.ReadLine());
-  int num2 = Convert.ToInt32(Console.ReadLine());
+  double num1 = Convert.ToDouble(Console.ReadLine());
+  double num2 = Convert.ToDouble(Console.ReadLine());
 
   Console.WriteLine("Select the number of the wanted operation");
   Console.WriteLine(@"
@@ -27,7 +28,7 @@ case "1":
   switch(operation) 
   {
    case "1":
-      int res = num1 + num2;
+      double res = num1 + num2;
       Console.WriteLine($"The Result of {num1} + {num2} is {res}");
      break;
 
@@ -61,12 +62,13 @@ case "2":
 
   Console.WriteLine("Select the number of the wanted trigonometric function");
   Console.WriteLine(@"
-    1-Sine       
-    2-Cosine   
-    3-Tangent
-    4-Cosecant
-    5-Secant
-    6-Cotangent");
+    1-Sine (Sin)       
+    2-Cosine (Cos)
+    3-Tangent (Tan)
+    4-Cosecant (Cossec)
+    5-Secant (Sec)
+    6-Cotangent (Cotan)
+    7-All");
 
   string TrigonometricFunction = Console.ReadLine() ?? "1";
 
@@ -100,7 +102,39 @@ case "2":
     case "6":
     Console.WriteLine($"The Coangent of {double3} is {1/(Math.Tan(radian3))}");
     break;
+        
+    case "7":
+    Console.WriteLine($"The Sine of {double3} is {Math.Sin(radian3)}");
+    Console.WriteLine($"The Cosine of {double3} is {Math.Cos(radian3)}");
+    Console.WriteLine($"The Tangent of {double3} is {Math.Tan(radian3)}");
+    Console.WriteLine($"The Cosecant of {double3} is {1/(Math.Sin(radian3))}");
+    Console.WriteLine($"The Secant of {double3} is {1/(Math.Cos(radian3))}");
+    Console.WriteLine($"The Coangent of {double3} is {1/(Math.Tan(radian3))}");
+    break;
   }
+
+break;
+case "3":
+  Console.WriteLine("Write the Number you will use");
+  
+  int num4 = Convert.ToInt32(Console.ReadLine());
+  
+  double double4 = double.Parse(num4.ToString());
+
+  Console.WriteLine("Select the number of the wanted function");
+  Console.WriteLine(@"
+    1-Inverse (1/x)");
+
+string Special = Console.ReadLine() ?? "1";       
+
+   switch(Special)
+   {
+
+    case "1":
+
+    Console.WriteLine($"The Inverse of {num4} is {1f/num4}");
+    break;
+   }
 
 break;
 }
